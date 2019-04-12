40d2ad1c47a9ca90e7ec0435284dc58abc2b61b4
brianisadog <s254901282002@gmail.com>
2019-04-11 18:50:44 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/page/precache-manifest.ccbb6038482df3ad7547e1194abd3b06.js"
+  "/page/precache-manifest.7ae4f9b0333d7fe246b56226ee634b76.js"
 );
 
 workbox.clientsClaim();
